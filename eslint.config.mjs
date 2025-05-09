439a2a730040135626075754e40c6aca9352396a
andres <stvandres1101@gmail.com>
2025-05-09 23:36:57 +0700

    deployandres3

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,6 +17,7 @@ const eslintConfig = [
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-explicit-any": "off",
       "react/no-unescaped-entities": "off",
+      "@next/next/no-img-element": "off",
     },
   },
 ];
